7ef249cca0aab181f561345cfafaa1a29ce304be
colme99 <c.colmenero.2017@alumnos.urjc.com>
2021-02-15 14:59:56 +0100

    URL corregida

--- demo/src/main/java/com/example/demo/CheckLoginController.java
+++ demo/src/main/java/com/example/demo/CheckLoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class CheckLoginController {
-	@GetMapping("/greeting")
+	@GetMapping("/checkLogin")
 	public String greeting(Model model, @RequestParam String nombreUsuario, @RequestParam String contraseña) {		
 		/*
 		 * Comprobar nombreUsuario y contraseña en la BBDD
