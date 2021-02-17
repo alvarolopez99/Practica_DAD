df54b5473c82a76ab81f21764a7c33a55a8ceebf
SergioE17 <sergioarevaloe17@gmail.com>
2021-02-17 21:16:06 +0100

    homepage

---
+++ demo/src/main/java/com/example/demo/Homepage.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Homepage {
+	
+	
+	/*Este método tiene que invocarse cuando se llame a la URL de homepage*/
+	
+	@GetMapping("/hello") //Podemos especificar cualquier html
+	public String helloUser() {
+		
+		return "hello-user.html";
+
+	}
+	
+
+}
