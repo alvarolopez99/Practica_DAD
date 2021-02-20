2f2fbce273f54f341988547781ad7bd1c4c0c58c
SergioE17 <sergioarevaloe17@gmail.com>
2021-02-20 21:11:16 +0100

    Acceso de usuario nuevo

--- demo/src/main/java/com/example/demo/Controllers/IndexController.java
+++ demo/src/main/java/com/example/demo/Controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.example.demo.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -23,7 +24,11 @@ public class IndexController {
 	}
 	
 	@GetMapping("/iniciada")
-	public String sesionIniciadaMain(Model model) {
+	public String sesionIniciadaMain(Model model, @RequestParam String nombreUsuario) {
+		
+		
+		model.addAttribute("name", nombreUsuario);
+		
 		return "iniciada";
 	}
 	
