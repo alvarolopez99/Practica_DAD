a471d3e289ca94838d1a44cde5c7d3c9a2bbfca3
colme99 <c.colmenero.2017@alumnos.urjc.com>
2021-02-25 12:17:43 +0100

    Unificar controladores de Login

--- demo/src/main/java/com/example/demo/Controllers/LoginController.java
+++ demo/src/main/java/com/example/demo/Controllers/LoginController.java
@@ -3,6 +3,8 @@ package com.example.demo.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -10,5 +12,38 @@ public class LoginController {
 	@GetMapping("/greeting")
 	public String greeting(Model model) {
 		return "Iniciar_Sesion_Template";
+	}
+	
+	@PostMapping("/checkLogin")
+	public String comprobarLogin(Model model, @RequestParam String nombre, @RequestParam String contraseña) {		
+	
+		String url = "Error_Login";
+		
+		/* COMENTADO HASTA INTRODUCIR BBDD
+		Optional<Usuario> usuario = usuario.findByNombre(nombre);
+		
+		if(usuario.isPresent()) {
+			
+			if (contraseña == usuario.getContraseña()) {
+				url = "Sesion_Iniciada_Template";
+			} else {
+				String error = "Contraseña errónea";
+				model.addAttribute("mensajeError", error);
+			}
+			
+		} else {
+			String error = "Nombre de usuario erróneo";
+			model.addAttribute("mensajeError", error);
+		}
+		
+		*/
+		
+		return url;
+	}
+	
+	@GetMapping("/iniciada")
+	public String sesionIniciada(Model model) {
+			
+		return "";
 	}
 }
