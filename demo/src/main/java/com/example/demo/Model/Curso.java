17184bcee8936e703416d93befa712712f0187bf
SergioE17 <sergioarevaloe17@gmail.com>
2021-03-09 19:08:43 +0100

    Revert "Uso de BD con SQL funcionando"
    This reverts commit feada85b1de32a62b76a1ce8b13d1a7cff040090.

--- demo/src/main/java/com/example/demo/Model/Curso.java
+++ demo/src/main/java/com/example/demo/Model/Curso.java
@@ -21,9 +21,6 @@ public class Curso {
 	@JsonIgnore
 	private ArrayList<Blob> archivo;
 	
-
-	private ArrayList<String> nombresArchivos;
-	
 	
 	@ManyToOne
 	private Usuario profesor;
@@ -70,15 +67,6 @@ public class Curso {
 	public void setArchivo(ArrayList<Blob> archivo) {
 		this.archivo = archivo;
 	}
-	
-	public ArrayList<String> getNombresArchivos() {
-		return nombresArchivos;
-	}
-
-	public void setNombresArchivos(ArrayList<String> nombresArchivos) {
-		this.nombresArchivos = nombresArchivos;
-	}
-	
 
 	public String getTitulo() {
 		return titulo;
