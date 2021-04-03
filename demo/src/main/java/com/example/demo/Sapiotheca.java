3790c7fb1c84c871c56126e701b98964e69dc37c
SergioE17 <sergioarevaloe17@gmail.com>
2021-04-03 13:23:03 +0200

    Pruebas sockets

--- demo/src/main/java/com/example/demo/Sapiotheca.java
+++ demo/src/main/java/com/example/demo/Sapiotheca.java
@@ -14,6 +14,51 @@ public class Sapiotheca {
 		SpringApplication.run(Sapiotheca.class, args);
 		
 		//FASE 2 - 11/03/2021
+		
+		
+		/*String host = "127.0.0.1";
+		int port = 9999;
+		
+		try {
+			//Leer de la entrada estandar, enviar al servidor por el socket, leer del socket e imprimir
+			Socket socket = new Socket(host, port);
+			
+			InputStreamReader InStrReaderStn = new InputStreamReader(System.in);
+			BufferedReader BRStdIn = new BufferedReader(InStrReaderStn);
+			
+			
+			InputStreamReader InStrReaderSocket = new InputStreamReader(socket.getInputStream());
+			BufferedReader BRSocketIn = new BufferedReader(InStrReaderSocket);
+			
+			PrintWriter PWSocketOut = new PrintWriter(socket.getOutputStream()); //Para escribir en el socket
+			
+			String line;
+			
+			while(!(line = BRStdIn.readLine()).equals("x")) {
+				
+				PWSocketOut.println("HelloServer");
+				
+				String response = BRSocketIn.readLine();
+				System.out.println();
+				
+			}
+			
+			
+			BRSocketIn.close();
+			socket.close();
+			PWSocketOut.close();
+			
+			
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+		}
+		*/
+
+
+		
+		
+		
 	}
 	
 }
