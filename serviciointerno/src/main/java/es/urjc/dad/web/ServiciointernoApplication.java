cbd25154c480e9ed62e8e5cb19d21cc08b4d52a9
pbayona <pbayonag@hotmail.com>
2021-04-08 11:35:33 +0200

    inicio rabbit

--- serviciointerno/src/main/java/es/urjc/dad/web/ServiciointernoApplication.java
+++ serviciointerno/src/main/java/es/urjc/dad/web/ServiciointernoApplication.java
@@ -9,14 +9,62 @@ import java.net.ServerSocket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import rabbit.Receiver;
+
 
 @SpringBootApplication
 public class ServiciointernoApplication {
 
-	public static void main(String[] args) {
+	static final String topicExchangeName = "spring-boot-exchange";
+
+	  static final String queueName = "spring-boot";
+
+	  @Bean
+	  Queue queue() {
+	    return new Queue(queueName, false);
+	  }
+
+	  @Bean
+	  TopicExchange exchange() {
+	    return new TopicExchange(topicExchangeName);
+	  }
+
+	  @Bean
+	  Binding binding(Queue queue, TopicExchange exchange) {
+	    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	  }
+
+	  @Bean
+	  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+	      MessageListenerAdapter listenerAdapter) {
+	    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+	    container.setConnectionFactory(connectionFactory);
+	    container.setQueueNames(queueName);
+	    container.setMessageListener(listenerAdapter);
+	    return container;
+	  }
+
+	  @Bean
+	  MessageListenerAdapter listenerAdapter(Receiver receiver) {
+	    return new MessageListenerAdapter(receiver, "receiveMessage");
+	  }
+
+	  public static void main(String[] args) throws InterruptedException {
+	    SpringApplication.run(ServiciointernoApplication.class, args).close();
+	  }
+	  
+	/*public static void main(String[] args) {
 		SpringApplication.run(ServiciointernoApplication.class, args);
 
 		final Logger LOGGER=LoggerFactory.getLogger(ServiciointernoApplication.class);
@@ -50,6 +98,6 @@ public class ServiciointernoApplication {
 
 
 	
-	}
+	}*/
 
 }
