1c769134e9c9ee6a2b3e542d9f7ef23fefb361e8
pbayona <pbayonag@hotmail.com>
2021-04-08 12:25:30 +0200

    servicios en servicio interno y continuacion rabbit

--- serviciointerno/src/main/java/es/urjc/dad/web/rabbit/Receiver.java
+++ serviciointerno/src/main/java/es/urjc/dad/web/rabbit/Receiver.java
@@ -8,9 +8,18 @@ public class Receiver {
 
 	private CountDownLatch latch = new CountDownLatch(1);	//Sincronizacion
 
-	  public void receiveMessage(String message) {
-	    System.out.println("Received <" + message + ">");
-	    //latch.countDown();
+	public void receiveMessage(String message) {
+		  if(message.contains("|*Image*|")) {
+			  message = message.substring(9);
+			  System.out.println("Received Reescale Petition<" + message + ">");
+		  }
+		  else if(message.contains("|*Mail*|")) {
+			  message = message.substring(7);
+			  System.out.println("Received Mail Send Petition <" + message + ">");
+		  }
+		  
+		  //Si no es de esos tipos, se ignora el mensaje...
+	    
 	  }
 
 	  public CountDownLatch getLatch() {
---
+++ serviciointerno/src/main/java/es/urjc/dad/web/services/ImageService.java
@@ -0,0 +1,44 @@
+package es.urjc.dad.web.services;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ImageService {
+
+	public byte[] resize(byte[] bytesIn, int scaledWidth, int scaledHeight, String formatName)
+            throws IOException {
+		
+
+        // reads input image
+		InputStream is = new ByteArrayInputStream(bytesIn);
+        BufferedImage inputImage = ImageIO.read(is);
+ 
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
+ 
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+        g2d.dispose();
+ 
+        
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(outputImage, formatName, baos);
+        byte[] bytesOut = baos.toByteArray();
+        
+        return bytesOut;
+    }
+ 
+}
