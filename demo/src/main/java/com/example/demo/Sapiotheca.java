b635f15d5d872adc661123dfc1ff8c83f4282809
colme99 <c.colmenero.2017@alumnos.urjc.es>
2021-05-01 10:53:06 +0200

    Manejador de caché y habilitar la caché en la aplicación

--- demo/src/main/java/com/example/demo/Sapiotheca.java
+++ demo/src/main/java/com/example/demo/Sapiotheca.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,22 +17,30 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.example.demo.services.FilterService;
 
-
+@EnableCaching
 @SpringBootApplication
 public class Sapiotheca {
 	
+	static final Log LOGGER = LogFactory.getLog(Sapiotheca.class);
 	
+	//FASE 2 - 11/03/2021
 	public static void main(String[] args) {
-		SpringApplication.run(Sapiotheca.class, args);
-			
-		//FASE 2 - 11/03/2021
-		final Logger LOGGER=LoggerFactory.getLogger(Sapiotheca.class);
-		
+		SpringApplication.run(Sapiotheca.class, args);		
 	}
+	
+    @Bean
+    public CacheManager cacheManager() {
+    	LOGGER.info("Activando caché...");
+    	return new ConcurrentMapCacheManager("anuncios");
+    }
 	
 }
