f663e89ee0850af2c843ccd17f950df750cc3449
colme99 <c.colmenero.2017@alumnos.urjc.es>
2021-05-02 14:43:04 +0200

    Caché en Anuncios y Chats

--- demo/src/main/java/com/example/demo/Repository/AnuncioRepository.java
+++ demo/src/main/java/com/example/demo/Repository/AnuncioRepository.java
@@ -1,18 +1,34 @@
 package com.example.demo.Repository;
 import java.util.List;
+import java.util.Optional;
 
 import javax.persistence.ManyToOne;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.Model.Anuncio;
 import com.example.demo.Model.Usuario;
 
+@CacheConfig(cacheNames="cacheSapiotheca")
 public interface AnuncioRepository extends JpaRepository<Anuncio, Integer> {
 	
-	//consultas personalizadas
+	@Cacheable
+	Optional<Anuncio> findById(int id);
+	
+	@Cacheable
+	List<Anuncio> findAll();
+	
+	@CacheEvict(allEntries=true)
+	Anuncio save(Anuncio anuncio);
+	
+	@CacheEvict(allEntries=true)			// Todas las entradas o sólo la que tenga ese id??
+	Anuncio deleteById(int id);
 	
 	
+	// Consultas personalizadas
 	
 	List<Anuncio> findByprofesor(Usuario profesor);
 
@@ -23,8 +39,9 @@ public interface AnuncioRepository extends JpaRepository<Anuncio, Integer> {
 	List<Anuncio> findByhorario(String horario);
 	
 	List<Anuncio> findByprecio(String precio);
-	
+
 	List<Anuncio> findBycurso(String curso);
+
 
 	//Generar la consulta a SQL automaticamente en base a esta informacion
 	//Interfaz de la clase que vamos a persistir
--- demo/src/main/java/com/example/demo/Repository/ChatRepository.java
+++ demo/src/main/java/com/example/demo/Repository/ChatRepository.java
@@ -2,17 +2,29 @@ package com.example.demo.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.Model.Anuncio;
 import com.example.demo.Model.Chat;
 import com.example.demo.Model.Usuario;
 
+@CacheConfig(cacheNames="cacheSapiotheca")
 public interface ChatRepository  extends JpaRepository<Chat, Long> {
 
+	@Cacheable
 	Optional<Chat> findById(long id);
+	
+	@CacheEvict(allEntries=true)
+	Chat save(Chat chat);
+	
+	@Cacheable
 	List<Chat> findByProfesor(Usuario u);
 	List<Chat> findByAlumno(Usuario u);
 	
+	@Cacheable
 	Optional<Chat> findByProfesorAndAlumno(Usuario u1, Usuario u2);
 	
 	
